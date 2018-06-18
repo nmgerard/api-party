2df251318af038adc58ef7ccdec341eb7d6f8648
Nikita Gerard <nikita.mgerard@gmail.com>
2018-06-18 14:44:24 -0400

    fix errors in CSS file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch, NavLink } from 'react-router-dom'
+import Github from './Github'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>API Party </h1>
+               <div className="App-header">
+          <h3>Ain't no party like an</h3>
+          <h1>API Party</h1>
+        </div>
 
-        <ul>
+        <ul className="navLinks">
           <li>
             <NavLink to="/github">GitHub</NavLink>
           </li>
         </ul>  
 
         <Switch>
-    <Route path = '/github' render={() => <h1>GitHub!</h1>}/>
+    <Route path = '/github' component={Github}/>
     <Route 
     render={() => (
     <p>To get started, click one of the links above.</p>
@@ -27,4 +31,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
---
+++ src/Github.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import { Route } from 'react-router-dom'
+
+class Github extends Component {
+    render(){
+        return(
+            <div> </div>
+        )
+    }
+}
+
+export default Github
