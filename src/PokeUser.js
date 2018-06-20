a671f84e5830109a80271b6f8b24808266372acd
Nikita Gerard <nikita.mgerard@gmail.com>
2018-06-20 14:17:37 -0400

    add pokemon's image to displayed results

--- src/PokeUser.js
+++ src/PokeUser.js
@@ -5,7 +5,9 @@ class PokeUser extends Component {
     super(props)
 
     this.state = {
-      poke: {}
+      poke: {
+        "sprites": {}
+      }
     }
 
     this.fetchUserData()
@@ -28,8 +30,9 @@ class PokeUser extends Component {
     const { poke } = this.state
     console.log(poke)
     return (
-      <div className="PokeUser" style={styles}>
-        {/* <img src={poke.sprites[back_default]} alt="" /> */}
+      <div className="PokeUser">
+        <img src={this.state.poke.sprites.front_default} alt="" style={styles.img}/>
+        <div style={styles.main}>
         <h2>
            Species Name: {poke.name} 
 
@@ -38,14 +41,22 @@ class PokeUser extends Component {
         <h3>Weight: {poke.weight}00 g</h3>
         <h3>Height: {poke.height}0 cm</h3>
         <h3>Base Experience: {poke.base_experience}</h3>
+        </div>
       </div>
     )
   }
 }
 
 const styles = {
+  main:{
     color: "red",
     backgroundImage: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6JJsLpcy24GKXZXe2DAPIgGEsG2iFksYQNvykVTU7SjCYMshhlA)',
+  },
+  img:{
+    backgroundColor: "black",
+    width: "80%",
+    maxWidth: "400px",
+  },
 }
 
 export default PokeUser
